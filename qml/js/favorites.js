567971fdf7ae6a6359847fe3bc770f711ef710c1
Niels Simonsen <nesnomis@gmail.com>
2015-08-25 09:36:19 +0200

    Set favorite sorting to title.

--- qml/js/favorites.js
+++ qml/js/favorites.js
@@ -19,7 +19,7 @@ function load(model)
     model.clear()
     db.transaction(function(tx)
     {
-        var rs = tx.executeSql('SELECT * FROM channels ORDER BY icon');
+        var rs = tx.executeSql('SELECT * FROM channels ORDER BY title');
         for(var i = 0; i < rs.rows.length; i++)
         {
             model.append({"source" : rs.rows.item(i).source, "title" : rs.rows.item(i).title,"site" : rs.rows.item(i).site,"section" : rs.rows.item(i).section,"icon" : rs.rows.item(i).icon})
--- rpm/harbour-allradio.spec
+++ rpm/harbour-allradio.spec
@@ -13,7 +13,7 @@ Name:       harbour-allradio
 %{!?qtc_make:%define qtc_make make}
 %{?qtc_builddir:%define _builddir %qtc_builddir}
 Summary:    AllRadio
-Version:    1.2.2
+Version:    1.2.3
 Release:    1
 Group:      Qt/Qt
 License:    GPL2.0
